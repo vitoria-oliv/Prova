770adcdf2f333925023823ecdc8824615b776436
vitoria oliveira <oliveiravitoriaroberta1@gmail.com>
2025-05-21 11:44:35 -0300

    adicionei musica e quiz

---
+++ prova/Backend/backend/backend/logicaProgramacao/01_variaveis.py
@@ -0,0 +1,34 @@
+nome= "Vitoria"
+idade= 17
+salario= 1512.50
+legal= True
+
+print( "Nome:" , nome)
+print( "Idade:" , idade)
+print("Salario:" , salario)
+print(" É legal?:" , legal)
+
+
+
+cidade= "Sorocaba"
+Idade= 300
+habitantes= 780.30
+sorocabaTemPraia= True
+
+print( "Cidade:" , cidade)
+print( "Idade:" , idade)
+print("Habitantes:" , habitantes)
+print(" Sorocaba tem praia?:" , sorocabaTemPraia)
+
+
+animal= "peixe boi"
+comprimento= 4
+peso= 480.50
+eleViveMaisDe100Anos= False
+
+print( "Animal:" , animal)
+print( "Comprimento:" , comprimento)
+print("Peso:" , peso)
+print(" Ele vive mais de 100 anos?:" , eleViveMaisDe100Anos)
+
+
---
+++ prova/Backend/backend/backend/logicaProgramacao/02_operacoesMatematicas.py
@@ -0,0 +1,48 @@
+a = 10
+b = 15
+soma = a + b
+
+print("Soma de a + b é:" , soma)
+
+c = 10
+d = 15
+subtracao = c - d 
+print("Subtracao de c - d é" , subtracao)
+
+e = 10 
+f = 15
+multiplicacao = e * f
+
+print("Multiplicacao de e * f é" , multiplicacao)
+
+g = 10 
+h = 15
+divisao = g / h
+
+print(" A divisao de g / h é" , divisao)
+
+
+y = 75
+p = 70
+soma = y + p 
+
+print (" A soma de y + p é:" , soma)
+
+q = 90
+l = 30
+subtracao = q - l 
+
+print ("A subtracao de q - l é:" , subtracao)
+
+s = 45
+t = 6
+multiplicacao = s * t 
+
+print ("A multiplicacao de s * t é:" , multiplicacao)
+
+m = 225
+v = 50
+divisao = m / v
+
+print (" A divisao de m / v é:" , divisao)
+
