405ad1a8e2576b15f78906f3c46e46378811fff3
Vitoria Oliveira <oliveiravitoriaroberta@gmail.com>
2025-06-11 11:49:36 -0300

    novas atividades

---
+++ prova/Backend/backend/backend/logicaProgramacao/08_lacos.py
@@ -0,0 +1,19 @@
+def ola (nome):
+    return "Ola, " + nome
+
+print(ola("joema"))
+
+def media(a,b):
+    return (a + b) / 2
+
+print(media(55, 78))
+
+def par (numero):
+    if  (numero % 2 == 0 ):
+    return print (" é par ")
+   else:
+
+    return print ("é impar")
+
+print (par(9))
+  
---
+++ prova/Backend/backend/backend/logicaProgramacao/_lacos/07_lacos.py
@@ -0,0 +1,29 @@
+# For
+i = 10
+lista = [10,15,33,42,55,60,73,88,91,100]
+for i in lista: 
+    print("Contando:" , i)
+    i+= 1
+
+
+numeros = [10,15,33,42,55,60,73,88,91,100]
+pares = 0
+
+for x in numeros:
+      if (x%2 == 0):
+           pares +=1
+
+
+
+
+
+# While    
+soma = 0
+valor = 1
+
+while valor != 0:
+      valor = int (input("Digite o seu valor:"))
+      soma +=valor
+     
+      
+print("valor total da soma:" , soma)
