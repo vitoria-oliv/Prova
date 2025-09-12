2300d769557e93565a877457b813b63a36c7bfe1
vitoria oliveira <oliveiravitoriaroberta1@gmail.com>
2025-09-12 08:42:00 -0300

    fiz uma atividade avaliativa

--- prova/Backend/backend/backend/logicaProgramacao/lista/25_atividade avaliativa.py
+++ prova/Backend/backend/backend/logicaProgramacao/lista/25_atividade avaliativa.py
@@ -4,68 +4,101 @@
 
 import json
 
+ARQUIVO_CATEGORIAS = 'categorias.json'
+ARQUIVO_PRODUTOS = 'produtos.json'
 
-arquivo_Produto = "produtos.json"
-id_counter = 0
+categorias_id = 0
+produtos_id = 0
 
-def carregarProdutos():
-    "Carrega os produtos de um arquivo JSON. Se o arquivo não existir, retorna uma lista vazia."
-    return json.load(open(arquivo_Produto, "r")) if os.path.exists(arquivo_Produto) else []
+categorias = []
+produtos = []
 
-def salvarProdutos(produtos):
-    "Salva os produtos em um arquivo JSON de forma organizada."
-    json.dump(produtos, open(arquivo_Produto, "w"), indent=2, ensure_ascii=False)
-
-def cadastrarProduto():
-    global id_counter
-    produtos = carregarProdutos()
-
-    print("\n--- Cadastro de Produto ---")
-    
-    nome = input("Nome do produto: ").strip()
-    if not nome:
-        print("Nome do produto não pode ser vazio.")
-        input("Pressione Enter para continuar...")
-        return
-        
+def carregar_arquivo(categorias):
     try:
-        preco_str = input("Digite o valor do produto: ").replace(",", ".")
-        preco = float(preco_str)
-    except ValueError:
-        print("Preço inválido. Por favor, digite um número.")
-        input("Pressione Enter para continuar...")
-        return
+        with open(categorias, 'r') as arquivo:
+            return json.load(arquivo)
+    except FileNotFoundError:
+        return []
 
-    id_counter += 1
-    novo_produto = {"id": id_counter, "nome": nome, "preco": preco}
-    
-    produtos.append(novo_produto)
-    salvarProdutos(produtos)
-    
-    print(f"\nProduto cadastrado: ID: {id_counter} | Nome: {nome} | Preço: R$ {preco:.2f}")
+def salvar_arquivo(nome, dados):
+    with open(nome, 'w') as arquivo:
+        json.dump(dados, arquivo, indent=4)
 
-def listaProdutos():
-    produtos = carregarProdutos()
+def exibir_menu():
+    print("\n- Loja a Descoberta de Tudo -")
+    print("1 -- Cadastrar categoria")
+    print("2 -- Cadastrar produto")
+    print("3 -- Listar categorias")
+    print("4 -- Listar produtos")
+    print("5 -- Sair")
+
+def cadastrar_categoria():
+    global categorias_id
+    global categorias
     
-    if not produtos:
-        print("\nNenhum produto cadastrado.")
+    nome_categoria = input("Nome da categoria: ")
+    categoria = {"id": categorias_id, "nome": nome_categoria}
+    categorias.append(categoria)
+    salvar_arquivo(ARQUIVO_CATEGORIAS, categorias)
+    categorias_id += 1
+
+    print("Categoria cadastrada!")
+
+def cadastrar_produto():
+    global produtos_id
+    global produtos
+    
+    nome_produto = input("Nome do produto: ")
+    preco = float(input("Preço: "))
+    id_categoria = int(input("ID da categoria: "))
+    produto = {
+        "id": produtos_id,
+        "nome": nome_produto,
+        "preco": preco,
+        "id_categoria": id_categoria
+    }
+    produtos.append(produto)
+    salvar_arquivo(ARQUIVO_PRODUTOS, produtos)
+    produtos_id += 1
+
+    print("Produto cadastrado!")
+
+def listar_categorias():
+    global categorias
+    if categorias:
+        print("CATEGORIAS:")
+        for categorias in categorias:
+            print(f"ID: {categorias['id']} - Nome: {categorias['nome']}")
     else:
-        print("\n----- Produtos -----")
-        for p in produtos:
-            print(f"ID: {p['id']} | Nome: {p['nome']} | Preço: R$ {p['preco']:.2f}")
+        print("Nenhuma categoria cadastrada.")
 
-def MenuPrincipal():
-    while True:
-        funcao = input("\n--- Loja a Descoberta de Tudo ---\n1. Cadastrar novo produto\n2. Listar Produtos\n3. Sair\nOpção: ")
-        
-        match funcao:
-            case "1":
-                cadastrarProduto()
-            case "2":
-                listaProdutos()
-            case "3":
-                print("\nSaindo...")
-                break
-            case _:
-                print("\nOpção inválida, tente novamente.")
-MenuPrincipal()
+def listar_produtos():
+    global produtos
+    global categorias
+    if produtos:
+        print("PRODUTOS:")
+        for produtos in produtos:
+            print(f"ID: {produtos['id']} - Nome: {produtos['nome']} - Preço: {produtos['preco']} - Categoria: {produtos['id_categoria']}") 
+    else:
+        print("Nenhum produto cadastrado.")
+
+categorias = carregar_arquivo(ARQUIVO_CATEGORIAS)
+produtos = carregar_arquivo(ARQUIVO_PRODUTOS)
+
+while True:
+    exibir_menu()
+    opcao = input("Escolha o número: ")
+
+    if opcao == '1':
+        cadastrar_categoria()
+    elif opcao == '2':
+        cadastrar_produto()
+    if opcao == '3':
+        listar_categorias()
+    elif opcao == '4':
+        listar_produtos()
+    elif opcao == '5':
+        print("Saindo... Até a proxima!")
+        break
+    else:
+        print("Opção inválida.")
