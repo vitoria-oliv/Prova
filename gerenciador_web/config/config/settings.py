c26c9f2f49dfc45a1260e20b4089f82600735432
vitoria oliveira <oliveiravitoriaroberta1@gmail.com>
2025-09-19 11:48:01 -0300

    fiz duas atividades

--- gerenciador_web/config/config/settings.py
+++ gerenciador_web/config/config/settings.py
@@ -116,6 +116,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "tarefas/static"
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- gerenciador_web/config/tarefas/views.py
+++ gerenciador_web/config/tarefas/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-
-# Create your views here.
-
 from .models import Tarefa
-from django.http import HttpResponse 
 
 def listar_tarefas(request):
+    # 1. a busca no banco de dados continua a mesma
     tarefas_salvas = Tarefa.objects.all()
-    print(tarefas_salvas)
-    return HttpResponse("View 'listar_tarefas' executada! Confira no terminal")
+
+    # 2. criamos um "dicionario do contexto" para enviar os dados ao template.
+    # A chave 'minhas_tarefas' será a variável que usaremos no html.
+    contexto = {
+        'minhas_tarefas': tarefas_salvas 
+    }
+
+    # 3. renderizamos o tamplate, passando a requisição e o contexto com os dados.
+    return render(request, 'tarefas/lista.html', contexto)
